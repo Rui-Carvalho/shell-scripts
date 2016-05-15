74621cbc1f71f7b6174fb4d1c0e40801c6d16461
Rui-Carvalho <ruionline@yahoo.com>
2016-05-15 22:01:13 +0100

    Fixed logic in main behaviour, Added response codes and refactored a bit

--- websiteMonitor/websiteMonitor.sh
+++ websiteMonitor/websiteMonitor.sh
@@ -4,57 +4,47 @@
 #                  SETTINGS
 # ===========================================
 source parse_yml.sh
-eval $(parse_yaml config.yml "config_")
-
-NOTIFY_EMAIL=rui.carvalho@businessoffashion.com
-ALERT_TO_EMAIL=alert@localhost
-WEBSITE=www.businessoffashion.com
-PAUSE=60
-FAILED=0
-DEBUG=1
+CONFIG=$(parse_yaml config.yml "config_")
+eval $CONFIG
+IS_DOWN=0
 
 # ===========================================
 #                  FUNCTIONS
 # ===========================================
 function main {
-    while true 
-    do
+    while true; do
         CURL_OUTPUT="Response code: %{http_code}" 
-        RESPONSE=$(/usr/bin/curl --write-out "$CURL_OUTPUT" --silent --output /dev/null $WEBSITE)
+        RESPONSE=$(/usr/bin/curl --write-out "$CURL_OUTPUT" --silent --output /dev/null $config_WEBSITE)
+        RESP_STATUS=$?
+        # libcurl error codes: https://curl.haxx.se/libcurl/c/libcurl-errors.html
         RESPONSE_CODE=${RESPONSE#Response code: }
-        
-        echo $RESPONSE_CODE
-
-        RESP_STATUS=$? # libcurl error codes: https://curl.haxx.se/libcurl/c/libcurl-errors.html
-
+         
         logStatus
 
-        # If the server is down and no alert is sent - alert
-        if [ $RESP_STATUS -ne 0 ] && [ $FAILED -eq 0 ]; then
+        # If the server has gone down and no alert has been sent
+        if ([ $RESP_STATUS -ne 0 ] && [ $RESPONSE_CODE -ne 200 ] && [ $IS_DOWN -eq 0 ]); then
             whenServerGoesDown
 
-        # If the server is back up and no alert is sent - alert
-        elif [ $RESP_STATUS -eq 0 ] && [ $FAILED -eq 1 ]; then
+        # If the server as gone back up and no alert has been sent
+        elif ([ $RESP_STATUS -eq 0 ] && [ $RESPONSE_CODE -eq 200 ] && [ $IS_DOWN -eq 1 ]) ; then
             whenServerGoesUp
         fi
-        sleep $PAUSE
+
+        sleep $config_INTERVAL
     done
 
 }
 
 function logStatus {
-    if [ $DEBUG -eq 1 ]
-    then
+    if [ $config_DEBUG -eq 1 ]; then
         echo -e "\n_____________________"
-        echo "STATUS = $RESP_STATUS"
-        echo "FAILED = $FAILED"
-        if [ $RESP_STATUS -ne 0 ]
-        then
-            echo "$WEBSITE ==> is down"
-
-        elif [ $RESP_STATUS -eq 0 ]
-        then
-            echo "$WEBSITE ==> is up"
+        echo "Response Code = $RESPONSE_CODE"
+        if [ $RESPONSE_CODE -eq 200 ]; then
+            echo "$config_WEBSITE ==> is up"
+        elif [ $RESPONSE_CODE -eq 000 ]; then
+            echo -e "Could not Resolve DNS for $config_WEBSITE \n$config_WEBSITE ==> is down"
+        else
+            echo "$config_WEBSITE ==> is down"
         fi
         echo -e "_____________________\n"
     fi
@@ -62,43 +52,44 @@ function logStatus {
 
 function whenServerGoesUp {
     sendEmailThroughGmail "BACK UP"
-    FAILED=1
+    IS_DOWN=0
 }
 
 function whenServerGoesDown {
     sendEmailThroughGmail "DOWN"
-    FAILED=0
+    IS_DOWN=1
 }
 
 function sendEmailThroughGmail {
     CONTENT_FILE="$HOME/mail.txt"
     URL="smtps://smtp.gmail.com:465"
 
-    GMAIL_EMAIL="$config_git_account_user"
-    GMAIL_APP_PASSWORD="$config_git_account_pass"
+    GMAIL_EMAIL="$config_GIT_ACCOUNT_USER"
+    GMAIL_APP_PASSWORD="$config_GIT_ACCOUNT_PASS"
 
-    MAIL_FROM="alerts@businessoffashion.com"
-    NAME_FROM="Rui Carvalho"
+    MAIL_FROM="$config_NOTIFY_FROM_EMAIL"
+    NAME_FROM="$config_NOTIFY_FROM_NAME"
     
-    MAIL_TO="rui.carvalho@businessoffashion.com"
-    NAME_TO="Alerts at BoF"
+    MAIL_TO="$config_NOTIFY_TO_EMAIL"
+    NAME_TO="$config_NOTIFY_TO_NAME"
 
-    SUBJECT="Website $WEBSITE id $1"
+    SUBJECT="Website $config_WEBSITE is $1"
 
     # ---EMAIL CONTENT---
-    read -r -d '' EMAIL_CONTENT <<- EOF
+read -r -d '' EMAIL_CONTENT <<- EOF
 From: "$NAME_FROM" <$MAIL_FROM>
 To: "$NAME_TO" <$MAIL_TO>
 Subject: $SUBJECT
 
 ALERT Report,
-The website $WEBSITE is $1. 
+$SUBJECT
 Check done at: `date`
 EOF
 
-    # ---SENDING THE EMAIL---
+    # ---SENDING THE EMAIL--- 
     echo "$EMAIL_CONTENT" > $CONTENT_FILE
 
+    # Note: use --verbose on curl to debug if necessary
     curl --ssl --digest --silent \
         --mail-from "$MAIL_FROM" \
         --mail-rcpt "$MAIL_TO" \
@@ -112,5 +103,4 @@ EOF
 # ===========================================
 #                  RUN MAIN
 # ===========================================
-sendEmailThroughGmail
-#main
+main
